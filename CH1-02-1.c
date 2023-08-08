d622eb822e12f5a660496cf6c04b962d7a356ffa
cd2dot0 <139972240+cd2dot0@users.noreply.github.com>
2023-08-08 12:38:28 +0200

    Add solution to exercise 1.2

---
+++ CH1-02-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main()
+{
+    printf("Hello, World\c");
+    printf("Hello, World\d");
+    printf("Hello, World\2");
+    printf("Hello, World\.");
+    printf("Hello, World\0");
+    printf("Hello, World\!");
+}
